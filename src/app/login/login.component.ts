b773077b6a03c23abc3675ae076784c1f8cbee29
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-12-07 16:56:25 -0800

    added basic joi schema

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  schema;
+  constructor() {}
 
-  constructor() { }
+  Joi = require("joi");
+  schema = Joi.object()
+    .keys({
+      username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+      password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+      access_token: [Joi.string(), Joi.number()],
 
-  ngOnInit() {
-  }
-
+      email: Joi.string().email({ minDomainAtoms: 2 })
+    })
+    .with("username")
+    .without("password", "access_token");
 }
